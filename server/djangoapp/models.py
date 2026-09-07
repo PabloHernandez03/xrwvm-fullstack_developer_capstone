88d34cb25646579b858079cee4b3f493295fec60
PabloHernandez03 <pablo.hernandez6287@alumnos.udg.mx>
2026-09-07 11:47:16 -0600

    Add CarMake and CarModel models with admin, populate and get_cars

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,51 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    """A car manufacturer. One make has many car models."""
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    """A specific model sold by a dealer, belonging to one CarMake."""
+
+    SEDAN = 'SEDAN'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    COUPE = 'COUPE'
+    HATCHBACK = 'HATCHBACK'
+
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (HATCHBACK, 'Hatchback'),
+    ]
+
+    # Many-To-One: one CarMake has many CarModels
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default=SUV)
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015),
+        ],
+    )
+    # Refers to a dealer stored in the Mongo backend, not a Django FK.
+    # Optional on purpose: populate.py creates models without a dealer.
+    dealer_id = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.car_make.name + " " + self.name
