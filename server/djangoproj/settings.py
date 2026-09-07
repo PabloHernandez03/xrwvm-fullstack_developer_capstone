ea42350670005f8173624282360d9a70e3d01f41
PabloHernandez03 <pablo.hernandez6287@alumnos.udg.mx>
2026-09-07 10:28:00 -0600

    Set lab host in ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,14 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'pablohernan4-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://pablohernan4-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
